40c9183c8be023704ad075704a1741ad49a74b58
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-11-26 14:50:45 +0100

    task 1 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import "package:http/http.dart";
+import "package:http/http.dart" as http;
 
 void main() {
   runApp(const MainApp());
+}
+// Hier möchte ich mir die Aufgaben holen
+
+Future<List<dynamic>> getTask() async {
+// hier die komplette Uri die ich aufteilen muss 'https://dummyjson.com/todos'
+
+  final Uri uri = Uri.https("dummyjson.com", "todos");
+// hier packe ich mir mit get das Ergebnis in die variable response // übergeben wird an get die Variable Uri
+  final http.Response response = await http.get(uri);
+// hier die Abfrage ob alles ok
+  if (response.statusCode == 200) {
+    final String data =
+        response.body; // den Body aus der Response in die Variable data
+
+//! warum String? muss der Key immer String sein?
+    final Map<String, dynamic> decodedJson = jsonDecode(
+      data,
+    ); // in die Map decodedJson packen wir die Daten von vorher aus dem Body aber decodiert
+
+    final tasks = decodedJson["todos"];
+    return tasks;
+  }
+// leere Liste bei Fehler noch ändern
+  return [];
 }
 
 class MainApp extends StatefulWidget {
@@ -13,7 +39,32 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  final String _response = "Klicke auf den Button für eine API-Abfrage";
+  String taskContents = "";
+  int taskId = 0;
+  int index = 0;
+  bool completed = false;
+  // Hier wird die Init State Methode am Anfang gesetzt und dann getFirstTask ausgeführt
+  @override
+  void initState() {
+    super.initState();
+    getFirstTask;
+  }
+// Hier eerstelle cih die Methode zum holen der ersten Aufgabe
+
+  void getFirstTask() async {
+    final List<dynamic> tasks = await getTask();
+    final Map<String, dynamic> firstTask = tasks[index];
+    final String firstTaskContent = firstTask["todo"];
+    final int taskNumber = firstTask["id"];
+    final bool complete = firstTask["completed"];
+    setState(() {
+      taskContents = firstTaskContent;
+      taskId = taskNumber;
+      completed = complete;
+      index++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,11 +73,19 @@ class _MainAppState extends State<MainApp> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text('Hello World!'),
-              SizedBox(
+              Text("Aufgabe $taskId"),
+              Text(taskContents),
+              completed
+                  ? const Text("Aufgabe erledigt")
+                  : const Text("Aufgabe noch offen"),
+              const SizedBox(
                 height: 50,
               ),
-              ElevatedButton(onPressed: () {}, child: const Text("Drück mich"))
+              ElevatedButton(
+                  onPressed: () {
+                    getFirstTask();
+                  },
+                  child: const Text("Drück mich"))
             ],
           ),
         ),
